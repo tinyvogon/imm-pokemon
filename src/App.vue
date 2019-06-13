59c4f2aa263bd157c11be618fa6a17a2cbd39ddd
Hoan Huynh <seal-loaf@Hoans-MacBook-Air.local>
2019-06-13 15:05:35 -0700

    added button functionality

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
     <div class="container">  
-      <h1 class="text-center display-1 m-5"> Rock Type Pokemon </h1>
+      <h2 class="text-center display-1 m-5 text-capitalize"> {{ activeType }} Type Pokemon </h2>
+      <span v-for="item in types" class="btn btn-primary m-1" v-on:click="somethingHappened(item.name)" :key="item">
+        {{item.name}}
+      </span>
+      <br><br>
       <div class="row">
-        <card :url="item.pokemon.url" v-for="item in pokemonOfRockType" :key="item"></card>
+        <card :url="item.pokemon.url" v-for="item in pokemonOfCurrentType" :key="item.pokemon.name"></card>
       </div>
     </div>
   </div>
@@ -21,21 +25,41 @@ export default {
   },
   data: function() {
     return {
-        pokemonOfRockType: ""
+        pokemonOfCurrentType: "",
+        types: "",
+        activeType: "rock"
         }
   },
-  mounted: function(){
-      console.log("mounted function ran");
+  methods:{
+    somethingHappened: function(name){
+      this.activeType = name;
+      this.retrievePokemonOfSpecifiedType(this.activeType)
+    },
+    retrievePokemonOfSpecifiedType: function(type){
       const axios = require('axios');
       const vm = this;
       axios({
         method: 'get',
-        url: 'https://pokeapi.co/api/v2/type/rock',
-        responseType: 'stream'
+        url: 'https://pokeapi.co/api/v2/type/' + type
         })
       .then(function (response) {
         //console.log(response.data.pokemon);
-        vm.pokemonOfRockType = response.data.pokemon
+        vm.pokemonOfCurrentType = response.data.pokemon
+        });
+    }
+  },
+  mounted: function(){
+      const axios = require('axios');
+      const vm = this;
+      console.log("mounted function ran");
+      this.retrievePokemonOfSpecifiedType(this.activeType)
+      axios({
+        method: 'get',
+        url: 'https://pokeapi.co/api/v2/type'
+        })
+      .then(function (response) {
+        //console.log(response.data);
+        vm.types = response.data.results
         });
   }  
 }
